9cc2eec5981cc8d76a6e1ea4a9c2d13f21e3e8b3
Pedro Amador <pedro.amador@gigigo.com>
2018-01-21 17:17:27 +0100

    Build: Changes in bin/test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -5,24 +5,24 @@ cd "$(dirname $0)/.."
 
 function restorePermissions() {
   docker run --rm -v `pwd`:/home ubuntu chown -R --reference=/home/Jenkinsfile /home # try to restore permissions
+  returnValue=$((returnValue + $?))
 }
 
+returnValue=0
 restorePermissions
 
 cp ci-scripts/test/cucumber/config.yml.dist ci-scripts/test/cucumber/config.yml
-returnValue=0
-docker-compose up -d --force-recreate
-returnValue=$((returnValue + $?))
-docker-compose exec php composer install
+docker-compose up -d --force-recreate || exit $?
+docker-compose exec -T php composer install
 returnValue=$((returnValue + $?))
 
-docker-compose exec test-runner rm -rf /opt/bddfire/ci-scripts/test/cucumber/reports
+docker-compose exec -T test-runner rm -rf /opt/bddfire/ci-scripts/test/cucumber/reports
 returnValue=$((returnValue + $?))
-docker-compose exec test-runner mkdir /opt/bddfire/ci-scripts/test/cucumber/reports
+docker-compose exec -T test-runner mkdir /opt/bddfire/ci-scripts/test/cucumber/reports
 returnValue=$((returnValue + $?))
-docker-compose exec test-runner bundle install --path vendor/
+docker-compose exec -T test-runner bundle install --path vendor/
 returnValue=$((returnValue + $?))
-docker-compose exec test-runner bundle exec cucumber -p poltergeist
+docker-compose exec -T test-runner bundle exec cucumber -p poltergeist
 returnValue=$((returnValue + $?))
 
 docker-compose down
