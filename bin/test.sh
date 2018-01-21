bff2ccede2d324de95c5f69ce0142c40aafb421a
Sergio Ortega <sergio.ortega.gomez@gmail.com>
2018-01-21 15:11:49 +0100

    Update test

--- bin/test.sh
+++ bin/test.sh
@@ -1,17 +1,24 @@
-#!/bin/sh
-
-#Enviroment UP
-docker-compose up -d
-
-# composer install
-docker-compose exec php composer install
+#!/bin/bash
 
 # Run interface test
-cd "$(dirname $0)/../ci-scripts/test/cucumber"
-cp config.yml.dist config.yml
-mkdir -p reports
-rm -f reports/*
-rake poltergeist
+cd "$(dirname $0)/.."
 
-#Enviromet Down
-docker-compose down
+function restorePermissions() {
+  docker run --rm -v `pwd`:/home ubuntu chown -R --reference=/home/Jenkinsfile /home # try to restore permissions
+}
+
+restorePermissions
+
+cp ci-scripts/test/cucumber/config.yml.dist ci-scripts/test/cucumber/config.yml
+
+docker-compose up -d --force-recreate || exit $?
+docker-compose exec php composer install || exit $?
+
+docker-compose exec test-runner rm -rf /opt/bddfire/ci-scripts/test/cucumber/reports || exit $?
+docker-compose exec test-runner mkdir /opt/bddfire/ci-scripts/test/cucumber/reports || exit $?
+docker-compose exec test-runner bundle install --path vendor/ || exit $?
+docker-compose exec test-runner bundle exec cucumber -p poltergeist || exit $?
+
+docker-compose down || exit $?
+
+restorePermissions
--- ci-scripts/test/cucumber/features/bddfire.feature
+++ ci-scripts/test/cucumber/features/bddfire.feature
@@ -2,5 +2,5 @@ Feature: Symfony 4 to explore BDDfire
 
 
 Scenario: View Hello World home page
-  Given I am on "http://nginx:20080"
+  Given I am on "http://nginx"
   Then I should see "Hello World"
