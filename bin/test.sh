9d39c67336305348864f884ef252c467e11121a1
Sergio Ortega <sergio.ortega.gomez@gmail.com>
2018-01-21 14:52:16 +0100

    Update test

--- bin/test.sh
+++ bin/test.sh
@@ -9,18 +9,9 @@ docker-compose exec php composer install
 # Run interface test
 cd "$(dirname $0)/../ci-scripts/test/cucumber"
 cp config.yml.dist config.yml
-if [ "$1" == "chrome" ] || [ "$1" == "poltergeist" ]
-then
-  # Local workstation test (chrome / PhantomJS)
-  # Remove phantomjs local storage
-  find "$HOME/Library/Application Support/Ofi Labs/PhantomJS" -maxdepth 1 -type f -name "*.localstorage" -delete
-  mkdir -p reports
-  rm -f reports/*
-  rake $1
-else
-  # Docker tests
-  bash -x docker.sh
-fi
+mkdir -p reports
+rm -f reports/*
+rake poltergeist
 
 #Enviromet Down
 docker-compose down
--- ci-scripts/test/cucumber/features/bddfire.feature
+++ ci-scripts/test/cucumber/features/bddfire.feature
@@ -2,5 +2,5 @@ Feature: Symfony 4 to explore BDDfire
 
 
 Scenario: View Hello World home page
-  Given I am on "http://nginx"
+  Given I am on "http://nginx:20080"
   Then I should see "Hello World"
