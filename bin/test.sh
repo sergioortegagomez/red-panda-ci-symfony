ea9b6532dad090cfa1cd8cb6ad00e65cacfd0cc3
Pedro Amador <pedro.amador@gigigo.com>
2018-01-21 16:35:09 +0100

    Build: Update test script

--- bin/test.sh
+++ bin/test.sh
@@ -10,15 +10,23 @@ function restorePermissions() {
 restorePermissions
 
 cp ci-scripts/test/cucumber/config.yml.dist ci-scripts/test/cucumber/config.yml
+returnValue=0
+docker-compose up -d --force-recreate
+returnValue=$((returnValue + $?))
+docker-compose exec php composer install
+returnValue=$((returnValue + $?))
 
-docker-compose up -d --force-recreate || exit $?
-docker-compose exec php composer install || exit $?
+docker-compose exec test-runner rm -rf /opt/bddfire/ci-scripts/test/cucumber/reports
+returnValue=$((returnValue + $?))
+docker-compose exec test-runner mkdir /opt/bddfire/ci-scripts/test/cucumber/reports
+returnValue=$((returnValue + $?))
+docker-compose exec test-runner bundle install --path vendor/
+returnValue=$((returnValue + $?))
+docker-compose exec test-runner bundle exec cucumber -p poltergeist
+returnValue=$((returnValue + $?))
 
-docker-compose exec test-runner rm -rf /opt/bddfire/ci-scripts/test/cucumber/reports || exit $?
-docker-compose exec test-runner mkdir /opt/bddfire/ci-scripts/test/cucumber/reports || exit $?
-docker-compose exec test-runner bundle install --path vendor/ || exit $?
-docker-compose exec test-runner bundle exec cucumber -p poltergeist || exit $?
-
-docker-compose down || exit $?
+docker-compose down
+returnValue=$((returnValue + $?))
 
 restorePermissions
+exit $returnValue
